a2206c23cb71feb00ee5a58c7a4715505dc66215
Incode <1206751678@qq.com>
2019-11-29 15:00:47 +0800

    spring boot websocket

---
+++ spring-boot-websocket/src/main/java/com/code/websocket/endpoint/MyWebSocket.java
@@ -0,0 +1,109 @@
+package com.code.websocket.endpoint;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author H
+ */
+@Component
+@ServerEndpoint(value = "/websocket")
+public class MyWebSocket {
+
+    /**
+     * concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+     */
+    private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<>();
+
+    /**
+     * 与某个客户端的连接会话，需要通过它来给客户端发送数据
+     */
+    private Session session;
+
+    /**
+     * 连接建立成功调用的方法
+     *
+     * @param session
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        // 加入 set 中
+        webSocketSet.add(this);
+        System.out.println("有新的连接加入！当前在线人数为：" + webSocketSet.size());
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sendMessage("连接成功，当前时间：" + sdf.format(System.currentTimeMillis()));
+        } catch (IOException e) {
+            System.out.println("IO 异常");
+        }
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        // 从 set 中删除
+        webSocketSet.remove(this);
+        System.out.println("有一连接关闭！当前在线人数为：" + webSocketSet.size());
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("来自客户端的消息：" + message);
+
+        // 群发消息
+        for (MyWebSocket item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 发生错误时调用
+     *
+     * @param session
+     * @param error
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("发生错误");
+        error.printStackTrace();
+    }
+
+
+    /**
+     * 发送消息
+     *
+     * @param message
+     * @throws IOException
+     */
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+        // this.session.getAsyncRemote().sendText(message);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
